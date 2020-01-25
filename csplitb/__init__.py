eb319bc9950b08ebbbfe30b8388ff0a062cb6049
Brian Pow <brianpow@gmail.com>
2020-01-25 22:57:20 +0800

    add support for ending pattern, program is now more verbose

--- csplitb/__init__.py
+++ csplitb/__init__.py
@@ -5,14 +5,18 @@ import mmap
 import os
 
 class CSplitB(object):
-    def __init__(self, spliton, infile, number, prefix, suffix):
+    def __init__(self, spliton, infile, splitend, number, prefix, suffix):
         spliton_str = binascii.unhexlify(spliton)
+        splitend_str = ""
+        if splitend is not None:
+            splitend_str = binascii.unhexlify(splitend)
         tmp = os.path.splitext(infile)
         if prefix is None:
             prefix = tmp[0] + "_"
         if suffix is None:
             suffix = tmp[1]
         self.spliton_str = spliton_str
+        self.splitend_str = splitend_str
         self.infile = infile
         self.number = number
         self.prefix = prefix
@@ -26,12 +30,12 @@ class CSplitB(object):
             while True:
                 idx = self.mm.find(self.spliton_str, self.last_idx + 1)
                 if idx == -1:
-                    self.write()
+                    return self.write()
                     break
                 else:
                     self.indexes.append(idx)
                     self.last_idx = idx
-
+        return 0
     def write(self):
         number = self.number
         if self.number is None:
@@ -40,11 +44,31 @@ class CSplitB(object):
 
         count = len(self.indexes)
         if len(self.indexes):
+            end_index = -1
+            file_written_count = 0
             for i in range(count - 1):
-                outfile = self.prefix + (number_fmt % (i+1) ) + self.suffix
-                self.do_write(self.mm[self.indexes[i]:self.indexes[i+1]], outfile)
-            outfile = self.prefix + (number_fmt % count ) + self.suffix
-            self.do_write(self.mm[self.indexes[count-1]:], outfile)
+                outfile = self.prefix + (number_fmt % (file_written_count+1) ) + self.suffix
+                if self.splitend_str:
+                    if self.indexes[i] < end_index:
+                        continue
+                    end_index=self.mm.find(self.splitend_str,self.indexes[i] + len(self.spliton_str) )+1
+                else:
+                    end_index=self.indexes[i+1]
+                if end_index > 0:
+                    print(self.indexes[i], end_index)
+                    self.do_write(self.mm[self.indexes[i]:end_index], outfile)
+                    file_written_count += 1
+            outfile = self.prefix + (number_fmt % (file_written_count + 1) ) + self.suffix
+            if self.splitend_str:
+                if self.indexes[count-1] < end_index:
+                    return file_written_count
+                end_index=self.mm.find(self.splitend_str, self.indexes[count-1] + len(self.spliton_str) )+1
+            else:
+                end_index=len(self.mm)
+            if end_index > 0:
+                self.do_write(self.mm[self.indexes[count-1]:end_index], outfile)
+                file_written_count += 1
+            return file_written_count
 
     def do_write(self, data, outfile):
         with open(outfile, "w+b") as f:
